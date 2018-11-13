8560ae88310f81d699c506b0ae3aad9cd9b3fab4
thuijbens <thuijbens1@gmail.com>
2018-11-13 16:37:08 +0100

    Add files via upload

--- HeartbeatArduinoAdafruit/HeartbeatArduinoAdafruit.ino
+++ HeartbeatArduinoAdafruit/HeartbeatArduinoAdafruit.ino
@@ -1,41 +1,21 @@
 #include <Adafruit_MQTT.h>
 #include <Adafruit_MQTT_Client.h>
-#include <Adafruit_MQTT_FONA.h>
-
-#include<LiquidCrystal.h>
 
 #include <Adafruit_CC3000.h>
 #include <ccspi.h>
 #include <SPI.h>
-#include <PubSubClient.h>
-#include "DHT.h"
 
 #define HEARTBEAT_PUBLISH_PATH "api/feeds/heartbeat/data/send.json"
 
 Adafruit_CC3000_Client client = Adafruit_CC3000_Client();
 mqttclient("io.adafruit.com", 1883, callback, client);
 
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+int k=0,rate=0;
 
-int in = 8;
-int Reset=6;
-int start=7;
-int count=0,i=0,k=0,rate=0;
+pinMode(D5, INPUT);
 
 unsigned long time2,time1;
 unsigned long time;
-
-byte heart[8] = 
-{
-  0b00000,
-  0b01010,
-  0b11111,
-  0b11111,
-  0b11111,
-  0b01110,
-  0b00100,
-  0b00000
-};
 
 void setup() {
   // put your setup code here, to run once:
